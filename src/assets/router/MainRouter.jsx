568856b4312fcd672cc419ca5ddf00e057096543
gayathri-2003-design <gayathrikh11@gmail.com>
2025-12-10 10:59:37 +0530

    Rename folder

--- src/assets/router/MainRouter.jsx
+++ src/assets/router/MainRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from '../pages/About';
-import Home from '../pages/Home';
+import About from '../pages/About.jsx';
+import Home from '../pages/Home.jsx';
 
 
 const MainRouter = () => {
